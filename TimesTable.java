0e8bf349b97e2a93123212a15d528ed6928c758a
Luke Thurston <luke.thurston@live.co.uk>
2019-02-04 11:12:39 +0000

    Create TimesTable.java

---
+++ TimesTable.java
@@ -0,0 +1,11 @@
+
+public class TimesTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 1; i < 20; i++) {
+			System.out.println("2 x " + i + " = " + (2*i));
+		}
+	}
+
+}
